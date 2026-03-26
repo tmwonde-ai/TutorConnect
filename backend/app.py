6d14161f7c497dc6e4214f67f764a6c0db3a067e
unknown <tmwonde@gmail.com>
2026-03-26 08:42:15 +0200

    the second backend fix

--- backend/app.py
+++ backend/app.py
@@ -39,7 +39,14 @@ db.init_app(app)
 
 CORS(
     app,
-    resources={r"/*": {"origins": ["http://localhost:3000", "https://my-frontend.vercel.app"]}},
+    resources={
+        r"/*": {
+            "origins": [
+                "http://localhost:3000",
+                "https://tutor-connect-five.vercel.app"
+            ]
+        }
+    },
     supports_credentials=True,
     methods=["GET", "POST", "OPTIONS"]
 )
