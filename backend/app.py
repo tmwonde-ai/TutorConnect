e0ed00e16e1b7f5f70d6e451ee076d8e2cfe024c
unknown <tmwonde@gmail.com>
2026-03-26 21:57:38 +0100

    admin improv

--- backend/app.py
+++ backend/app.py
@@ -22,6 +22,7 @@ app = Flask(__name__)
 # Fix Railway DATABASE_URL
 database_url = os.getenv('DATABASE_URL', 'sqlite:///tutoring.db')
 
+
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
@@ -291,6 +292,34 @@ class SessionMessage(db.Model):
 
     session = db.relationship('Session', backref=db.backref('messages', lazy='dynamic'))
 
+
+
+
+admin = User(
+    email=os.getenv("ADMIN_EMAIL"),
+    full_name=os.getenv("ADMIN_NAME"),
+    role="admin"
+)
+
+admin.set_password(os.getenv("ADMIN_PASSWORD"))
+
+
+
+existing_admin = User.query.filter_by(email=os.getenv("ADMIN_EMAIL")).first()
+
+if not existing_admin:
+    admin = User(
+        email=os.getenv("ADMIN_EMAIL"),
+        full_name=os.getenv("ADMIN_NAME"),
+        role="admin"
+    )
+    admin.set_password(os.getenv("ADMIN_PASSWORD"))
+
+    db.session.add(admin)
+    db.session.commit()
+    print("Admin created successfully!")
+else:
+    print("Admin already exists")
 
 # ==================== AUTHENTICATION MIDDLEWARE ====================
 
