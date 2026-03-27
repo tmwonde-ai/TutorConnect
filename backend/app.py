f6f3bd766ae06acb818adc12e87e8d7eb5b53183
unknown <tmwonde@gmail.com>
2026-03-27 10:44:31 +0100

    the init fix

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,6 @@ from sqlalchemy.orm import joinedload
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 import base64
-import eventlet
 
 
 load_dotenv()
@@ -54,14 +53,14 @@ CORS(
 
 # SocketIO with CORS allowed
 
-eventlet.monkey_patch()
 
 socketio = SocketIO(
     app,
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://tutor-connect-five.vercel.app"
-    ]
+    ],
+    async_mode="threading"
 )
 
 
@@ -307,24 +306,9 @@ class SessionMessage(db.Model):
     session = db.relationship('Session', backref=db.backref('messages', lazy='dynamic'))
 
 
-with app.app_context():
-    db.create_all()  # ensures tables exist
+ # ensures tables exist
 
-    existing_admin = User.query.filter_by(email=os.getenv("ADMIN_EMAIL")).first()
-
-    if not existing_admin:
-        admin = User(
-            email=os.getenv("ADMIN_EMAIL"),
-            full_name=os.getenv("ADMIN_NAME"),
-            role="admin"
-        )
-        admin.set_password(os.getenv("ADMIN_PASSWORD"))
-
-        db.session.add(admin)
-        db.session.commit()
-        print("Admin created successfully!")
-    else:
-        print("Admin already exists")
+ 
 
 
 # ==================== AUTHENTICATION MIDDLEWARE ====================
--- backend/init_db.py
+++ backend/init_db.py
@@ -1,7 +1,24 @@
 # init_db.py
 
-from app import app, db
+from app import app, db, User
+import os
 
 with app.app_context():
     db.create_all()
     print("Tables created!")
+
+    existing_admin = User.query.filter_by(email=os.getenv("ADMIN_EMAIL")).first()
+
+    if not existing_admin:
+        admin = User(
+            email=os.getenv("ADMIN_EMAIL"),
+            full_name=os.getenv("ADMIN_NAME"),
+            role="admin"
+        )
+        admin.set_password(os.getenv("ADMIN_PASSWORD"))
+
+        db.session.add(admin)
+        db.session.commit()
+        print("Admin created successfully!")
+    else:
+        print("Admin already exists")
